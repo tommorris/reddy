131a35e4dc38bad72186eaec094fdda7a9a83df0
Tom Morris <tom@tommorris.org>
2008-02-11 22:59:06 +0000

    rewrote W3C tests script as I now understand how the test suite is laid out. alas, the suite still fails

--- tests/test-w3c.rb
+++ tests/test-w3c.rb
@@ -2,33 +2,54 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), "../lib")
 require 'rena'
 
-def test (rdf, nt)
+def filename_to_contenttype (string)
+  if string =~ /\S+\.rdf/
+    format = "application/rdf+xml"
+  end
+  if string =~ /\S+\.nt/
+    format = "text/ntriples"
+  end
+  return format
+end
+
+def test (root, expected, actual)
   begin
-    rdfmodel = Rena::MemModel.new
-    rdfmodel.load(open(rdf), :content_type => "application/rdf+xml")
+    expectedmodel = Rena::MemModel.new
+    expectedmodel.load(open(root + expected), :content_type => filename_to_contenttype(root + expected))
   rescue
-    print "rdf/xml whoopsie!\n"
+    print "parsing error on Expected file: " + root + expected + "\n"
   end
 
   begin
-    ntmodel = Rena::MemModel.new
-    ntmodel.load(open(nt), :content_type => "text/ntriples")
+    actualmodel = Rena::MemModel.new
+    actualmodel.load(open(root + actual), :content_type => filename_to_contenttype(root + actual))
   rescue
-    print "ntriples whoopse!\n"
+    print "parsing error on Actual file: " + root + actual + "\n"
   end
 
-  if rdfmodel.to_ntriples == ntmodel.to_ntriples
+  if expectedmodel.to_ntriples == actualmodel.to_ntriples
     return true
   else
+    puts "EXPECTED: " + expectedmodel.to_ntriples
+    puts "ACTUAL: " + actualmodel.to_ntriples
     return false
   end
 end
 
-def testboth(string)
-  return test(string + ".rdf", string + ".nt").to_s
-end
+pre = "approved_20031114/"
+puts "amp-in-url 001: " + test(pre + "amp-in-url/test001.", "rdf", "nt").to_s
+puts "datatypes 001: " + test(pre + "datatypes/test001.", "rdf", "nt").to_s
+puts "datatypes 002: " + test(pre + "datatypes/test002.", "rdf", "nt").to_s
+puts "datatypes 003: " + test(pre + "datatypes/test003", "a.nt", "b.nt").to_s
+puts "datatypes 005: " + test(pre + "datatypes/test005", "a.nt", "b.nt").to_s
+puts "datatypes 008: " + test(pre + "datatypes/test008", "a.nt", "b.nt").to_s
+puts "datatypes 009: " + test(pre + "datatypes/test009", "a.nt", "b.nt").to_s
+puts "datatypes 011: " + test(pre + "datatypes/test011", "a.nt", "b.nt").to_s
+puts "pfps-10 001: " + test(pre + "pfps-10/test001", "a.nt", "b.nt").to_s
 
-root = "approved_20031114/"
-puts "amp-in-url 001: " + testboth(root + "amp-in-url/test001")
-puts "datatypes 001: " + testboth(root + "datatypes/test001")
-puts "datatypes 002: " + testboth(root + "datatypes/test002")
+# I decided that handling this manually is easier than parsing the RDF.
+# I put two TextMate snippets to make entrance of these lines easier:
+# rdt:
+# puts "$1 $2: " + test(pre + "$1/test${2}$5.", "${3:rdf}", "${4:nt}").to_s
+# rdtt:
+# puts "$1 $2: " + test(pre + "$1/test${2}$5", "${3:a.nt}", "${4:b.nt}").to_s
