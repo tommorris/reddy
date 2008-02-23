144cfc20c2be7f4c8ed0009eeb6ced4c1a832d98
Tom Morris <tom@iMac.local>
2008-02-23 20:36:19 +0000

    started turning W3C-2003 tests into unit tests

--- tests/units.rb
+++ tests/units.rb
@@ -12,10 +12,29 @@ class TestTypedLiterals < Test::Unit::TestCase
   
   def test_literal
     string = Rena::TypedLiteral.new("hullo", "http://www.w3.org/2001/XMLSchema#string")
-    assert_equal(string.to_s, "hullo")
-    assert_equal(string.type, "http://www.w3.org/2001/XMLSchema#string")
+    assert_equal string.to_s, "hullo"
+    assert_equal string.type, "http://www.w3.org/2001/XMLSchema#string"
     integer = Rena::TypedLiteral.new("01", "http://www.w3.org/2001/XMLSchema#int")
-    assert_equal(integer.to_s, "1")
-    assert_equal(integer.type, "http://www.w3.org/2001/XMLSchema#int")
+    assert_equal integer.to_s, "1"
+    assert_equal integer.type, "http://www.w3.org/2001/XMLSchema#int"
+  end
+  
+  def test_amp_in_url
+    # tests to see how ampersands in URLs are handled
+    intest = Rena::MemModel.new
+    intest.load("approved_20031114/amp-in-url/test001.rdf", :content_type => "application/rdf+xml")
+    outtest = Rena::MemModel.new
+    outtest.load("approved_20031114/amp-in-url/test001.nt", :content_type => "text/ntriples")
+    assert_equal intest.to_ntriples, outtest.to_ntriples
+  end
+  
+  def test_xsd_decimal_integer_compatible_datatypes_intensional
+    # implements datatypes_intensional/test001.nt
+    # rdfs:comment = The claim that xsd:integer is a subClassOF xsd:decimal 
+    #      is not incompatible with using the intensional semantics for datatypes.
+    assert_nothing_thrown do
+      intest = Rena::MemModel.new
+      intest.load("approved_20031114/datatypes-intensional/test001.nt", :content_type => "text/ntriples")
+    end
   end
 end
