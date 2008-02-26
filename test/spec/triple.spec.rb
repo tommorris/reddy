55e5d8470cd9a9bc823d20500127e3bf7531a435
Tom Morris <tom@tommorris.org>
2008-02-26 12:52:13 +0000

    added a test for NTriple creation

--- test/spec/triple.spec.rb
+++ test/spec/triple.spec.rb
@@ -7,7 +7,7 @@ describe "Triples" do
   it "should have a subject" do
     f = Triple.new(BNode.new, URIRef.new('http://xmlns.com/foaf/0.1/knows'), BNode.new)
     f.subject.class.should == BNode
-    puts f.to_ntriples
+    # puts f.to_ntriples
   end
   it "should require that the subject is a URIRef or BNode" do
   end
@@ -20,5 +20,7 @@ describe "Triples" do
   it "should require that the object is a URIRef, BNode, Literal or Typed Literal" do
   end
   it "should emit an NTriple" do
+    f = Triple.new(URIRef.new("http://tommorris.org/foaf#me"), URIRef.new("http://xmlns.com/foaf/0.1/name"), Literal.new("Tom Morris"))
+    f.to_ntriples.should == "<http://tommorris.org/foaf#me> <http://xmlns.com/foaf/0.1/name> \"Tom Morris\" ."
   end
 end
