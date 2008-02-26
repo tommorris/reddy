6e37d678220e789decaae4081c707181d2db6a1e
Tom Morris <tom@tommorris.org>
2008-02-26 12:52:28 +0000

    Started speccing out a Namespaces class

---
+++ test/spec/namespaces.spec.rb
@@ -0,0 +1,8 @@
+describe "Namespaces" do
+  it "should have a URI" do
+  end
+  it "should have an XML and N3-friendly prefix" do
+  end
+  it "should be able to attach to the graph for subsitution" do
+  end
+end
