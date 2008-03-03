50efa79acc3e2daed106a978bd0e2d41145f3dd9
Tom Morris <tom@tommorris.org>
2008-03-03 17:06:49 +0000

    started work on Graph class - we can add triples, which is a neat start

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,9 +1,15 @@
+require 'lib/triple'
 class Graph
-  def initialize(args)    
+  attr_accessor :triples
+  
+  def initialize
+    @triples = []
   end
   
-  def add_triple(args)
+  def add_triple(s, p, o)
+    @triples + [ Triple.new(s, p, o) ]
   end
   
-  alias << add_triple
+#  alias (<<, add_triple)
+#  alias (=+, add_triple)
 end
--- test/spec/graph.spec.rb
+++ test/spec/graph.spec.rb
@@ -1,4 +1,15 @@
+require 'lib/bnode'
+require 'lib/uriref'
+require 'lib/triple'
+require 'lib/literal'
+require 'lib/graph'
 describe "Graphs" do
+  it "should allow you to add one or more triples" do
+    lambda do
+      f = Graph.new
+      f.add_triple(BNode.new, URIRef.new("http://xmlns.com/foaf/0.1/knows"), BNode.new)
+    end.should_not raise_error
+  end
   it "should support << as an alias for add_triple" do
     pending "Graphs not implemented yet"
   end
