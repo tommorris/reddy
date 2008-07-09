c2a8a08609c3acbf906269216fc0307d695c29f3
Tom Morris <tom@tommorris.org>
2008-07-09 11:57:49 +0100

    removed call to test_string

--- lib/uriref.rb
+++ lib/uriref.rb
@@ -4,7 +4,6 @@ require 'lib/exceptions/uri_relative_exception'
 class URIRef
   attr_accessor :uri
   def initialize (string)
-    self.test_string(string)
     @uri = Addressable::URI.parse(string)
     if @uri.relative?
       raise UriRelativeException, "<" + @uri.to_s + ">"
