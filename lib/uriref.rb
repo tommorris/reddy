218085726c748b9b99531b68b7ccda46e548f705
Tom Morris <tom@tommorris.org>
2008-07-09 11:45:55 +0100

    decided to get rid of support for standard uri library - Addressable is so much better.

--- lib/uriref.rb
+++ lib/uriref.rb
@@ -5,11 +5,7 @@ class URIRef
   attr_accessor :uri
   def initialize (string)
     self.test_string(string)
-    if Addressable.nil?
-      @uri = URI.parse(string)
-    else
-      @uri = Addressable::URI.parse(string)
-    end
+    @uri = Addressable::URI.parse(string)
     if @uri.relative?
       raise UriRelativeException, "<" + @uri.to_s + ">"
     end
