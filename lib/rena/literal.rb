7a04c2f49356ca911cb6fe125aec69d9da3f3586
Piers Cawley <pdcawley@bofh.org.uk>
2008-08-16 00:15:50 +0100

    Tidied up literal...

--- lib/rena/literal.rb
+++ lib/rena/literal.rb
@@ -55,11 +55,9 @@ module Rena
     end
 
     def to_trix
-      out = if @lang
-              "<plainLiteral xml:lang=\"#{@lang.value}\">"
-            else
-              "<plainLiteral>"
-            end
+      out = @lang \
+              ? "<plainLiteral xml:lang=\"#{@lang.value}\">" \
+              : "<plainLiteral>"
       out += @contents
       out += "</plainLiteral>"
       return out
@@ -68,51 +66,42 @@ module Rena
   end
 
   class TypedLiteral < Literal
+    LITERAL_ENCODING = "http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral"
+    INTEGER_ENCODING = "http://www.w3.org/2001/XMLSchema#int"
+    FLOAT_ENCODING   = "http://www.w3.org/2001/XMLSchema#float"
+    STRING_ENCODING  = "http://www.w3.org/2001/XMLSchema#string"
+
     attr_accessor :contents, :encoding
     def initialize(contents, encoding)
       @contents = contents
       @encoding = encoding
-      if @encoding == "http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral"
-        @xmlliteral = true
-      else
-        @xmlliteral = false
-      end
     end
 
     def == (obj)
-      if obj.class == TypedLiteral && obj.contents == @contents && obj.encoding == @encoding
-        return true
-      else
-        return false
-      end
+      obj.class == self.class && obj.contents == @contents && obj.encoding == @encoding
     end
 
     def to_n3
-      if @encoding == "http://www.w3.org/2001/XMLSchema#int"
-        out = @contents.to_s
-      else
-        out = "\"" + @contents.to_s + "\""
-      end
+      out = @encoding == INTEGER_ENCODING ? @contents.to_s : "\"#{@contents}\""
       out += "^^<" + @encoding + ">" if @encoding != nil
       return out
     end
 
     def to_trix
-      "<typedLiteral datatype=\"" + @encoding + "\">" + @contents + "</typedLiteral>"
+      "<typedLiteral datatype=\"#{@encoding}\">#{@contents}</typedLiteral>"
     end
 
     def xmlliteral?
-      @xmlliteral
+      @encoding == LITERAL_ENCODING
     end
 
     def infer!
-      if @contents.class == Fixnum
-        @encoding = "http://www.w3.org/2001/XMLSchema#int"
-      elsif @contents.class == Float 
-        @encoding = "http://www.w3.org/2001/XMLSchema#float"
-      else
-        @encoding = "http://www.w3.org/2001/XMLSchema#string"
-      end
+      @encoding =
+        case @contents
+        when Integer; INTEGER_ENCODING
+        when Float;   FLOAT_ENCODING
+        else          STRING_ENCODING
+        end
     end
   end
 end
