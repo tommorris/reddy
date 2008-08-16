3b32dd19abf0059c406d11bd933aadf195607c70
Tom Morris <tom@tommorris.org>
2008-08-16 13:33:19 +0100

    made the tests slightly bigger

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,47 +4,36 @@ include Rena
 # w3c test suite: http://www.w3.org/TR/rdf-testcases/
 
 describe "RDF/XML Parser" do
-  # it "should be able to detect whether an XML file is indeed an RDF file" do
-  #   bad_doc = "<?xml version=\"1.0\"?><RDF><foo /></RDF>"
-  #   bad_graph = RdfXmlParser.new(bad_doc)
-  #   bad_graph.is_rdf?.should == false
-  #   
-  #   good_doc = "<?xml version=\"1.0\"?><rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"></rdf:RDF>"
-  #   good_graph = RdfXmlParser.new(good_doc)
-  #   good_graph.is_rdf?.should == true
-  # end
-  
   it "should be able to parse a simple single-triple document" do
     sampledoc = <<-EOF;
 <?xml version="1.0" ?>
-    <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-    xmlns:ex="http://www.example.org/" xml:lang="en" xml:base="http://www.example.org/foo">
-      <rdf:Description rdf:about="#" ex:name="bar">
-        <ex:belongsTo rdf:resource="http://tommorris.org/" />
-        <ex:sampleText rdf:datatype="http://www.w3.org/2001/XMLSchema#string">foo</ex:sampleText>
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+xmlns:ex="http://www.example.org/" xml:lang="en" xml:base="http://www.example.org/foo">
+  <rdf:Description rdf:about="http://example.org/joe" ex:name="bar">
+    <ex:belongsTo rdf:resource="http://tommorris.org/" />
+    <ex:sampleText rdf:datatype="http://www.w3.org/2001/XMLSchema#string">foo</ex:sampleText>
+    <ex:hadADodgyRelationshipWith>
+      <rdf:Description>
+        <ex:name>Tom</ex:name>
         <ex:hadADodgyRelationshipWith>
           <rdf:Description>
-            <ex:name>Tom</ex:name>
+            <ex:name>Rob</ex:name>
             <ex:hadADodgyRelationshipWith>
               <rdf:Description>
-                <ex:name>Rob</ex:name>
-                <ex:hadADodgyRelationshipWith>
-                  <rdf:Description>
-                    <ex:name>Mary</ex:name>
-                  </rdf:Description>
-                </ex:hadADodgyRelationshipWith>
+                <ex:name>Mary</ex:name>
               </rdf:Description>
             </ex:hadADodgyRelationshipWith>
           </rdf:Description>
         </ex:hadADodgyRelationshipWith>
       </rdf:Description>
-    </rdf:RDF>
+    </ex:hadADodgyRelationshipWith>
+  </rdf:Description>
+</rdf:RDF>
     EOF
     
     graph = RdfXmlParser.new(sampledoc)
-#    graph.is_rdf?.should == true
-#    graph.graph.size == 6
-#    print graph.graph.to_ntriples
+    graph.graph.size == 6
+    print graph.graph.inspect
   end
   # 
   # it "should raise an error if rdf:aboutEach is used, as per the negative parser test rdfms-abouteach-error001 (rdf:aboutEach attribute)" do
