a298b8c37944388e9b6085ed676ac6c0527aaa46
Tom Morris <tom@tommorris.org>
2008-09-29 19:58:42 +0100

    Further work on the parser

--- lib/rena/rdfxmlparser.rb
+++ lib/rena/rdfxmlparser.rb
@@ -1,194 +1,129 @@
-require 'ruby-debug'
 require 'lib/rena'
+require 'ruby-debug'
+require 'xml'
 include Rena
 
 module Rena
-  if require 'xml'
-    include LibXML
-    class RdfXmlParser
-      SYNTAX_BASE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-      RDF_TYPE = SYNTAX_BASE + "type"
-      RDF_DESCRIPTION = SYNTAX_BASE + "Description"
+  include LibXML
+  class RdfXmlParser
+    SYNTAX_BASE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    RDF_TYPE = SYNTAX_BASE + "type"
+    RDF_DESCRIPTION = SYNTAX_BASE + "Description"
 
-      attr_accessor :xml, :graph
-      def initialize(xml_str, uri = nil)
-        @excl = ["http://www.w3.org/1999/02/22-rdf-syntax-ns#resource",
-                 "http://www.w3.org/1999/02/22-rdf-syntax-ns#nodeID",
-                 "http://www.w3.org/1999/02/22-rdf-syntax-ns#about",
-                 "http://www.w3.org/1999/02/22-rdf-syntax-ns#ID"]
-        if uri != nil
-          @uri = Addressable::URI.parse(uri)
-        end
-        @graph = Rena::Graph.new
-        @xml = LibXML::XML::Parser.string(xml_str).parse
-        root = @xml.root
-        if is_rdf_root?(root)
-          parse_descriptions(root)
-        else
-          root.each {|n|
-            if is_rdf_root?(n)
-              parse_descriptions(n)
-            end
-          }
-        end
-      end
-    
-      private
-      def is_rdf_root? (node)
-        if node.namespace_node.href == SYNTAX_BASE && node.name == "RDF"
-          return true
-        else
-          return false
-        end
-      end
-      
-      private
-      def parse_descriptions (node)
-        node.each_element { |el|
-          # detect a subject
-          subject = URIRef.new(el.attributes.get_attribute_ns(SYNTAX_BASE, "about").value)
-          
-          # find a class
-          unless el.name == "Description" && el.namespace_node.href == SYNTAX_BASE
-            @graph.add_triple(subject, RDF_TYPE, url_helper(el.name, el.namespace_node.href, el.base))
+    attr_accessor :xml, :graph
+    def initialize(xml_str, uri = nil)
+      @excl = ["http://www.w3.org/1999/02/22-rdf-syntax-ns#resource",
+               "http://www.w3.org/1999/02/22-rdf-syntax-ns#nodeID",
+               "http://www.w3.org/1999/02/22-rdf-syntax-ns#about",
+               "http://www.w3.org/1999/02/22-rdf-syntax-ns#ID"]
+      @uri = Addressable::URI.parse(uri) unless uri.nil?
+      @graph = Rena::Graph.new
+      @xml = LibXML::XML::Parser.string(xml_str).parse
+      root = @xml.root
+      if is_rdf_root?(root)
+        parse_descriptions(root)
+      else
+        root.each {|n|
+          if is_rdf_root?(n)
+            parse_descriptions(n)
           end
-          
-          # read each attribute that's not in SYNTAX_BASE
-          el.attributes.each { |att|
-            @graph.add_triple(subject, url_helper(att.name, att.ns.href, el.base), att.value) unless att.ns.href == SYNTAX_BASE
-          }
-          
-          el.each_element {|child|
-            # puts child
-            # parse child elements
-            # puts child.to_s
-            # puts child.value
-            # child.children.each { |i|
-            #   puts i.class
-            # }
-          }
         }
       end
-
-      protected
-      def parse_rdf_doc
-        while @xml.read == 1
-          parse_descriptions(@xml.name) if @xml.node_type == 1 && @xml.name =~ /RDF$/
-        end
-      end
-      
-      private
-      def extract_name(str)
-        if str !=~ /(.+)\:(.+)/
-          return str
-        else
-          return name.match(/[\:|](.+)$/)[1]
-        end
-      end
-      
-      def url_helper(name, ns, base = nil)
-        if base.nil?
-          ns + extract_name(name)
-        else
-          create_uri(ns + extract_name(name), base)
-        end
-      end
-    
-      # def parse_descriptions (node_name)
-      #   # debugger
-      #   counter = @xml.depth
-      #   while @xml.read == 1 && @xml.depth >= counter 
-      #     if @xml.node_type == 1
-      #     
-      #       if is_new_node?(@xml.depth)
-      #         if @xml.has_attributes?
-      #           parsed_attributes = Hash.new
-      #           rdf_ns_uri = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-      #           while @xml.move_to_next_attribute == 1
-      #             case
-      #             when @xml.namespace_uri == rdf_ns_uri && @xml.name =~ /about$/
-      #               parsed_attributes[:about] = create_uri(@xml.value, @xml.base_uri)
-      #             when @xml.namespace_uri == rdf_ns_uri && @xml.name =~ /nodeID$/
-      #               parsed_attributes[:nodeID] = BNode.new @xml.value
-      #             # when # ID
-      #             # when 
-      #             end
-      #           end
-      #           if parsed_attributes[:about]
-      #             currentsubject = parsed_attributes[:about]
-      #           elsif parsed_attributes[:nodeID]
-      #             currentsubject = parsed_attributes[:nodeID]
-      #           elsif parsed_attributes[:ID]
-      #             currentsubject = parsed_attributes[:ID]
-      #           else
-      #             currentsubject = BNode.new
-      #           end
-      #         else
-      #           currentsubject = BNode.new
-      #         end
-      #         
-      #         @xml.move_to_first_attribute
-      #         while @xml.move_to_next_attribute == 1
-      #           unless @xml.name =~ /^rdf:(.+)$/
-      #             # TODO: make this not suck
-      #             @graph.add_triple(currentsubject, url_helper(@xml.name, @xml.namespace_uri), Literal.untyped(@xml.value))
-      #           end
-      #         end
-      #         # detect class
-      #         
-      #         parse_contents(currentsubject)
-      #         # debugger
-      #       end
-      #     end
-      #   end
-      # end
-      # 
-      # def parse_contents(currentsubject)
-      #   counter = @xml.depth
-      #   while @xml.read == 1 && counter >= @xml.depth
-      #     if @xml.node_type == 1
-      #       # debugger
-      #       subject = currentsubject # may vary later
-      #       predicate = url_helper(@xml.name, @xml.namespace_uri)
-      #       if @xml.has_attributes?
-      #         rdf_ns_uri = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-      #         while @xml.move_to_next_attribute == 1
-      #           if @xml.namespace_uri == rdf_ns_uri && @xml.name =~ /resource$/
-      #             object = create_uri(@xml.value, @xml.base_uri)
-      #             # puts object.to_s
-      #           elsif @xml.namespace_uri == rdf_ns_uri && @xml.name =~ /datatype$/
-      #             type_uri = @xml.value
-      #             while @xml.read == 1 && @xml.node_type != 15
-      #               if @xml.node_type == 3
-      #                 value = @xml.value
-      #                 break
-      #               end
-      #             end
-      #             object = Literal.typed(value, type_uri)
-      #           end
-      #         end
-      #       else
-      #         unless @xml.value.nil?
-      #           object = Literal.untyped(@xml.value)
-      #         end
-      #       end
-      # 
-      #       unless object.nil?
-      #         @graph.add_triple(subject, predicate, object)
-      #       end
-      #       # debugger
-      #     end
-      #   end
-      # end
-      
-      def create_uri (uri, base)
-        begin
-          a = URIRef.new(uri)
-        rescue UriRelativeException
-          a = URIRef.new(base) + URIRef.new(uri)
-        end
-        return a
+    end
+  
+    private
+    def is_rdf_root? (node)
+      if node.namespace_node.href == SYNTAX_BASE && node.name == "RDF"
+        return true
+      else
+        return false
       end
     end
+    
+    private
+    def parse_subject(el)
+      if el.attributes.get_attribute_ns(SYNTAX_BASE, "about")
+        return URIRef.new(el.attributes.get_attribute_ns(SYNTAX_BASE, "about").value)
+      elsif el.attributes.get_attribute_ns(SYNTAX_BASE, "ID")
+        return url_helper("#" + el.attributes.get_attribute_ns(SYNTAX_BASE, "ID").value, "", el.base)
+      elsif el.attributes.get_attribute_ns(SYNTAX_BASE, "nodeID")
+        return BNode.new(el.attributes.get_attribute_ns(SYNTAX_BASE, "nodeID").value)
+      else
+        return BNode.new
+      end
+    end
+    
+    private
+    def parse_descriptions (node, subject = nil)
+      node.each_element { |el|
+        # detect a subject
+        subject = parse_subject(el) if subject.nil?
+        
+        # find a class
+        unless el.name == "Description" && el.namespace_node.href == SYNTAX_BASE
+          @graph.add_triple(subject, RDF_TYPE, url_helper(el.name, el.namespace_node.href, el.base))
+        end
+        
+        # read each attribute that's not in SYNTAX_BASE
+        el.attributes.each { |att|
+          @graph.add_triple(subject, url_helper(att.name, att.ns.href, el.base), att.value) unless att.ns.href == SYNTAX_BASE
+        }
+        
+        el.each_element {|child|
+          predicate = url_helper(child.name, child.namespace_node.href, child.base)
+          object = child.content
+          #debugger
+            child.each {|contents|
+              if contents.text? and contents.content.strip.length != 0
+                @graph.add_triple(subject, predicate, contents.content)
+              end
+            }
+            child.each_element {|cel|
+              object = parse_subject(cel)
+              if child.attributes.get_attribute_ns(SYNTAX_BASE, "parseType")
+                case child.attributes.get_attribute_ns(SYNTAX_BASE, "parseType").value
+                when "XMLLiteral"; @graph.add_triple(subject, predicate, Literal.typed(cel.namespaced_to_s, "http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral"))
+                when "Literal"; @graph.add_triple(subject, predicate, cel.to_s)
+                when "Resource"
+                  resource = BNode.new
+                  @graph.add_triple(subject, predicate, resource)
+                  parse_descriptions(cel, resource)
+                #when "Collection";
+                end
+              else
+                @graph.add_triple(subject, predicate, object)
+                parse_descriptions(cel.parent, object)
+              end
+            }
+        }
+      }
+    end
+          
+    private
+    def extract_name(str)
+      if str !=~ /(.+)\:(.+)/
+        return str
+      else
+        return name.match(/[\:|](.+)$/)[1]
+      end
+    end
+    
+    def url_helper(name, ns, base = nil)
+      if ns != "" and !ns.nil?
+        a = Addressable::URI.parse(ns) + Addressable::URI.parse(name)
+      else
+        a = Addressable::URI.parse(name)
+      end
+      if a.relative?
+        if !base.nil?
+          a = Addressable::URI.parse(base) + a
+        elsif !@uri.nil?
+          a = @uri + a
+        end
+      end
+      
+      return URIRef.new(a.to_s)
+    end
+    
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,40 +1,40 @@
 require 'lib/rena'
+require 'ruby-debug'
 include Rena
 
 # w3c test suite: http://www.w3.org/TR/rdf-testcases/
 
 describe "RDF/XML Parser" do
-  it "should be able to parse a simple single-triple document" do
-    sampledoc = <<-EOF;
-<?xml version="1.0" ?>
-<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-xmlns:ex="http://www.example.org/" xml:lang="en" xml:base="http://www.example.org/foo">
-  <ex:Thing rdf:about="http://example.org/joe" ex:name="bar">
-    <ex:belongsTo rdf:resource="http://tommorris.org/" />
-    <ex:sampleText rdf:datatype="http://www.w3.org/2001/XMLSchema#string">foo</ex:sampleText>
-    <ex:hadADodgyRelationshipWith>
-      <rdf:Description>
-        <ex:name>Tom</ex:name>
-        <ex:hadADodgyRelationshipWith>
-          <rdf:Description>
-            <ex:name>Rob</ex:name>
-            <ex:hadADodgyRelationshipWith>
-              <rdf:Description>
-                <ex:name>Mary</ex:name>
-              </rdf:Description>
-            </ex:hadADodgyRelationshipWith>
-          </rdf:Description>
-        </ex:hadADodgyRelationshipWith>
-      </rdf:Description>
-    </ex:hadADodgyRelationshipWith>
-  </ex:Thing>
-</rdf:RDF>
-    EOF
-    
-    graph = RdfXmlParser.new(sampledoc)
-    graph.graph.size == 6
-    print graph.graph.to_ntriples
-  end
+#   it "should be able to parse a simple single-triple document" do
+#     sampledoc = <<-EOF;
+# <?xml version="1.0" ?>
+# <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+# xmlns:ex="http://www.example.org/" xml:lang="en" xml:base="http://www.example.org/foo">
+#   <ex:Thing rdf:about="http://example.org/joe" ex:name="bar">
+#     <ex:belongsTo rdf:resource="http://tommorris.org/" />
+#     <ex:sampleText rdf:datatype="http://www.w3.org/2001/XMLSchema#string">foo</ex:sampleText>
+#     <ex:hadADodgyRelationshipWith>
+#       <rdf:Description>
+#         <ex:name>Tom</ex:name>
+#         <ex:hadADodgyRelationshipWith>
+#           <rdf:Description>
+#             <ex:name>Rob</ex:name>
+#             <ex:hadADodgyRelationshipWith>
+#               <rdf:Description>
+#                 <ex:name>Mary</ex:name>
+#               </rdf:Description>
+#             </ex:hadADodgyRelationshipWith>
+#           </rdf:Description>
+#         </ex:hadADodgyRelationshipWith>
+#       </rdf:Description>
+#     </ex:hadADodgyRelationshipWith>
+#   </ex:Thing>
+# </rdf:RDF>
+#     EOF
+#     
+#     graph = RdfXmlParser.new(sampledoc)
+#     graph.graph.size.should == 9
+#   end
   # 
   # it "should raise an error if rdf:aboutEach is used, as per the negative parser test rdfms-abouteach-error001 (rdf:aboutEach attribute)" do
   #   sampledoc = <<-EOF;
@@ -124,47 +124,47 @@ xmlns:ex="http://www.example.org/" xml:lang="en" xml:base="http://www.example.or
   #   end.should raise_error
   # end
   # 
-  # it "should handle parseType=Literal according to xml-literal-namespaces-test001.rdf test" do
-  #   sampledoc = <<-EOF;
-  #   <?xml version="1.0"?>
-  # 
-  #   <!--
-  #     Copyright World Wide Web Consortium, (Massachusetts Institute of
-  #     Technology, Institut National de Recherche en Informatique et en
-  #     Automatique, Keio University).
-  # 
-  #     All Rights Reserved.
-  # 
-  #     Please see the full Copyright clause at
-  #     <http://www.w3.org/Consortium/Legal/copyright-software.html>
-  # 
-  #     Description: Visibly used namespaces must be included in XML
-  #            Literal values. Treatment of namespaces that are not 
-  #            visibly used (e.g. rdf: in this example) is implementation
-  #            dependent. Based on example from Issues List.
-  # 
-  # 
-  #     $Id: test001.rdf,v 1.2 2002/11/22 13:52:15 jcarroll Exp $
-  # 
-  #   -->
-  #   <rdf:RDF xmlns="http://www.w3.org/1999/xhtml"
-  #      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-  #      xmlns:html="http://NoHTML.example.org"
-  #      xmlns:my="http://my.example.org/">
-  #      <rdf:Description rdf:ID="John_Smith">
-  #       <my:Name rdf:parseType="Literal">
-  #         <html:h1>
-  #           <b>John</b>
-  #         </html:h1>
-  #      </my:Name>
-  # 
-  #     </rdf:Description>
-  #   </rdf:RDF>
-  #   EOF
-  #   
-  #   graph = RdfXmlParser.new(sampledoc, "http://www.w3.org/2000/10/rdf-tests/rdfcore/rdfms-xml-literal-namespaces/test001.rdf")
-  #   graph.graph.to_ntriples.should == "<http://www.w3.org/2000/10/rdf-tests/rdfcore/rdfms-xml-literal-namespaces/test001.rdf#John_Smith> <http://my.example.org/Name> \"<html:h1>\n            <b>John</b>\n          </html:h1>\"^^<http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral> .\n"
-  # end
+  it "should handle parseType=Literal according to xml-literal-namespaces-test001.rdf test" do
+    sampledoc = <<-EOF;
+<?xml version="1.0"?>
+  
+    <!--
+      Copyright World Wide Web Consortium, (Massachusetts Institute of
+      Technology, Institut National de Recherche en Informatique et en
+      Automatique, Keio University).
+  
+      All Rights Reserved.
+  
+      Please see the full Copyright clause at
+      <http://www.w3.org/Consortium/Legal/copyright-software.html>
+  
+      Description: Visibly used namespaces must be included in XML
+             Literal values. Treatment of namespaces that are not 
+             visibly used (e.g. rdf: in this example) is implementation
+             dependent. Based on example from Issues List.
+  
+  
+      $Id: test001.rdf,v 1.2 2002/11/22 13:52:15 jcarroll Exp $
+  
+    -->
+    <rdf:RDF xmlns="http://www.w3.org/1999/xhtml"
+       xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+       xmlns:html="http://NoHTML.example.org"
+       xmlns:my="http://my.example.org/">
+       <rdf:Description rdf:ID="John_Smith">
+        <my:Name rdf:parseType="Literal">
+          <html:h1>
+            <b>John</b>
+          </html:h1>
+       </my:Name>
+  
+      </rdf:Description>
+    </rdf:RDF>
+    EOF
+    
+    graph = RdfXmlParser.new(sampledoc, "http://www.w3.org/2000/10/rdf-tests/rdfcore/rdfms-xml-literal-namespaces/test001.rdf")
+    graph.graph.to_ntriples.should == "<http://www.w3.org/2000/10/rdf-tests/rdfcore/rdfms-xml-literal-namespaces/test001.rdf#John_Smith> <http://my.example.org/Name> \"<html:h1>\n            <b>John</b>\n          </html:h1>\"^^<http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral> .\n"
+  end
   # 
   # it "should pass rdfms-syntax-incomplete-test001" do
   #   sampledoc = <<-EOF;
