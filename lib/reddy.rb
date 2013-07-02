825a475f9a0371a1ac68561766be3af66888e3e3
Tom Morris <tom@tommorris.org>
2013-07-02 09:20:02 +0100

    removing all the things

--- lib/reddy.rb
+++
@@ -1,8 +0,0 @@
-#$:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-$:.unshift(File.dirname('reddy'))
-Dir.glob(File.join(File.dirname(__FILE__), 'reddy/**.rb')).each { |f| require f }
-
-module Reddy
-  VERSION = '0.1.0'
-end
